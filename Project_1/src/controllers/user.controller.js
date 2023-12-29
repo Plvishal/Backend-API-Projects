85da47bfe7ae886c2ca1697098b9a3ae4a05becb
Vishal <vishalpal1501@gmail.com>
2023-12-29 11:11:29 +0530

    First write all steps for the user register

--- Project_1/src/controllers/user.controller.js
+++ Project_1/src/controllers/user.controller.js
@@ -1,9 +1,15 @@
 import { asyncHandler } from '../utils/asyncHandler.js';
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: 'ok',
-  });
+  // 1.get user details from the frontend
+  // 2. validation- Not empty
+  // 3. Check if user already exixts:(username,email)
+  // 4.Check images , check for avatar
+  // 5. Upload them to cloudinary, check avatar upload or not on cloudinary
+  // 6. Create user object-create entry in database
+  // 7. remove password and refresh token field from the response
+  // 8. Chech for user creation
+  // 9. return response
 });
 
 export { registerUser };
