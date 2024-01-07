b95a06c2d78e88bd697d2e98e6a7215d0c273776
Vishal <vishalpal1501@gmail.com>
2024-01-08 02:15:35 +0530

    Playlist routes done

---
+++ Project_1/src/routes/playlist.routes.js
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import {
+  addVideoToPlaylist,
+  createPlaylist,
+  deletePlaylist,
+  getPlaylistById,
+  getUserPlaylist,
+  removeVideoFromPlaylist,
+  updatePlaylist,
+} from '../controllers/playlist.controller.js';
+import { verifyJWT } from '../middlewares/auth.middleware.js';
+
+const playlistRouter = Router();
+playlistRouter.use(verifyJWT);
+playlistRouter.route('/').post(createPlaylist);
+playlistRouter
+  .route('/:playlistId')
+  .get(getPlaylistById)
+  .patch(updatePlaylist)
+  .delete(deletePlaylist);
+
+playlistRouter.route('/add/:videoId/:playlistId').patch(addVideoToPlaylist);
+playlistRouter
+  .route('/remove/:videoId/:playlistId')
+  .patch(removeVideoFromPlaylist);
+
+playlistRouter.route('/user/:userId').get(getUserPlaylist);
