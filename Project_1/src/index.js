30807db66c3385bb44f3c35dd34c885a1a9fe489
Vishal <vishalpal1501@gmail.com>
2024-01-07 23:59:55 +0530

    f

--- Project_1/src/index.js
+++ Project_1/src/index.js
@@ -8,8 +8,6 @@ import {} from 'dotenv/config.js';
 
 import connectDB from './db/db.js';
 import { app } from './app.js';
-
-
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
